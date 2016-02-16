dff3f1a99e24048fd8e680771329b09e6a89d004
Jamu Kakar <jkakar@kakar.ca>
2016-02-15 18:22:37 -0800

    Add support for multiple Fernet secrets.

--- lib/fernet_ecto/type.ex
+++ lib/fernet_ecto/type.ex
@@ -1,15 +1,46 @@
 defmodule Fernet.Ecto.Type do
   def encrypt(plaintext) do
+    encrypt(plaintext, secret)
+  end
+
+  defp encrypt(plaintext, secret) when is_binary(secret) do
+    {:ok, _iv, ciphertext} = Fernet.generate(message: plaintext,
+                                             secret: secret)
+    {:ok, ciphertext}
+  end
+
+  defp encrypt(plaintext, secrets) when is_list(secrets) do
+    [secret|_] = secrets
     {:ok, _iv, ciphertext} = Fernet.generate(message: plaintext,
                                              secret: secret)
     {:ok, ciphertext}
   end
 
   def decrypt(ciphertext) do
+    decrypt(ciphertext, secret)
+  end
+
+  defp decrypt(ciphertext, secret) when is_binary(secret) do
     Fernet.verify(token: ciphertext,
                   secret: secret,
                   enforce_ttl: enforce_ttl,
                   ttl: ttl)
+  end
+
+  defp decrypt(_ciphertext, []) do
+    raise "incorrect mac"
+  end
+
+  defp decrypt(ciphertext, secrets) when is_list(secrets) do
+    [secret|rest] = secrets
+    try do
+      Fernet.verify(token: ciphertext,
+                    secret: secret,
+                    enforce_ttl: enforce_ttl,
+                    ttl: ttl)
+    rescue
+      RuntimeError -> decrypt(ciphertext, rest)
+    end
   end
 
   defp secret do
