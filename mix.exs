c2cff50de4c3a678c2e3100daf220b6c6735603c
Jamu Kakar <jkakar@kakar.ca>
2019-10-14 10:29:00 -0700

    build: Bump dependency versions (#15)
    Bump dependencies to the latest versions and include support for Ecto 3.0.

--- mix.exs
+++ mix.exs
@@ -37,10 +37,10 @@ defmodule Fernet.Ecto.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:earmark, "~> 1.1", only: [:dev]},
-     {:ex_doc, "~> 0.15.0", only: [:dev]},
-     {:ecto, "~> 2.2.0"},
-     {:fernetex, "~> 0.3.0"}]
+    [{:earmark, "~> 1.4", only: [:dev]},
+     {:ex_doc, "~> 0.21.2", only: [:dev]},
+     {:ecto, "~> 2.2 or ~> 3.0"},
+     {:fernetex, "~> 0.3.1"}]
   end
 
   defp package do
